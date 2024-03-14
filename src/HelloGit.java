b062ac77b1f987cef53eeedd49afd3c54e3a2dbe
cjh <1222014535@njupt.edu.cn>
2024-03-14 11:26:00 +0800

    Initial commit

---
+++ src/HelloGit.java
@@ -0,0 +1,11 @@
+/**
+ * @author chen
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/3/14 10:56
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello github");
+    }
+}
